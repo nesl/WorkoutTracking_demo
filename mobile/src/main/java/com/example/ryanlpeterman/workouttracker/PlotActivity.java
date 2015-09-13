551ae5a760336da71f2c7a1fdc052b145ae1c557
Bo-Jhang <TimeStringAlpha@gmail.com>
2015-09-12 23:02:11 -0700

    Make plotting part in real-time with 1-sec delay
    The phone now can plot the watch gyro data in realtime with 1 second
    delay, it's awesome now. I'm trying to do push-up detection, too, but
    haven't finished yet.

--- mobile/src/main/java/com/example/ryanlpeterman/workouttracker/PlotActivity.java
+++ mobile/src/main/java/com/example/ryanlpeterman/workouttracker/PlotActivity.java
@@ -10,6 +10,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -25,6 +26,8 @@ import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
 
+import java.util.ArrayList;
+
 public class PlotActivity extends AppCompatActivity implements SensorEventListener {
     private static final int HISTORY_SIZE = 250;
     private SensorManager sensorMgr = null;
@@ -35,6 +38,10 @@ public class PlotActivity extends AppCompatActivity implements SensorEventListen
     private SimpleXYSeries xHistorySeries = null;
     private SimpleXYSeries yHistorySeries = null;
     private SimpleXYSeries zHistorySeries = null;
+
+    private ArrayList<SensorData> sensorDataBuffer = new ArrayList<>();
+
+    private Handler addSampleHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,7 +105,15 @@ public class PlotActivity extends AppCompatActivity implements SensorEventListen
         MessageReceiver messageReceiver = new MessageReceiver();
         LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, messageFilter);
 
-        //startService(new Intent(this, MessageFromWearListenService.class));  // no need for this
+        // execute adding sample proxy
+        addSampleHandler.postDelayed(new DisplayOneSample(), 20L);
+    }
+
+    private boolean stopFlag = false;
+
+    @Override
+    public void onStop() {
+        stopFlag = true;
     }
 
     private void cleanup() {
@@ -157,10 +172,81 @@ public class PlotActivity extends AppCompatActivity implements SensorEventListen
 
         // redraw the Plots:
         gyroPlot.redraw();
+
+        // push-up detection
+        detectPushUp();
     }
 
+    private void detectPushUp() {
+        if (xHistorySeries.size() >= 30) {
+            float[] winGyroZ = new float[30];
+            float[] winGyroMag = new float[30];
+            int sidx = xHistorySeries.size() - 30;
+            for (int i = 0; i < 30; i++) {
+                float tx = xHistorySeries.getY(sidx + i).floatValue();
+                float ty = yHistorySeries.getY(sidx + i).floatValue();
+                float tz = zHistorySeries.getY(sidx + i).floatValue();
+                winGyroZ[i] = tz;
+                winGyroMag[i] = (float)Math.sqrt(tx * tx + ty * ty + tz * tz);
+            }
+/*
+            for (int j = 5; j < 20; j++) {
+                if winGyroZ[j] == max(win_gyro_z((j-2):(j+2)))
+            %fprintf('get j=%d\n', j);
+            for k = (j+1):(W-5)
+            if win_gyro_mag(k) == min(win_gyro_mag((k-2):(k+2))) ...
+            && win_gyro_mag(k) <= 0.8
+                    %fprintf('get j=%d k=%d\n', j, k);
+            for l = (k+1):(W-2)
+            if win_gyro_z(l) == min(win_gyro_z((l-2):(l+2))) ...
+            && 6 < l - j && l - j < 15 ...
+            && 1.3 <= win_gyro_z(j) && win_gyro_z(j) <= 4 ...
+            && -4 <= win_gyro_z(l) && win_gyro_z(l) <= -1
+            cur_center = i+k-1;
+            if cur_center ~= last_center_point
+                    %push_up_filter = 'o';
+            sec = gyro(i+k-1, 1);
+            acc_idx = find(acc(:, 1) > sec);
+            acc_idx = acc_idx(1);
+            PHW = 30;
+            cnt = sum(downward_deg((acc_idx-PHW):(acc_idx+PHW)) <= 20);
+            ratio = cnt / (PHW * 2 + 1);
+            pass = 'o';
+            if ratio < 0.7
+            pass = '.';
+            end
+            fprintf('i=%d, j=%d, k=%d, l=%d (%c)\n', ...
+            i, i+j-1, i+k-1, i+l-1, pass);
+            last_center_point = cur_center;
+            end
+                    j = W;
+            k = W;
+            l = W;
+            end
+                    end
+            end
+                    end
+            end
+                    end*/
+        }
+    }
+    private float winMax(float[] arr, int sidx, int eidx) {
+        float maxVal = arr[sidx];
+        for (int i = sidx + 1; i < eidx; i++)
+            if (arr[i] > maxVal)
+                maxVal = arr[i];
+        return maxVal;
+    }
 
-    public class MessageReceiver extends BroadcastReceiver {
+    private float winMin(float[] arr, int sidx, int eidx) {
+        float minVal = arr[sidx];
+        for (int i = sidx + 1; i < eidx; i++)
+            if (arr[i] > minVal)
+                minVal = arr[i];
+        return minVal;
+    }
+
+    private class MessageReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             byte[] bytes = intent.getByteArrayExtra("sensor_data");
@@ -169,12 +255,47 @@ public class PlotActivity extends AppCompatActivity implements SensorEventListen
 
             DataWindow window = DataWindow.uncompress(bytes);
 
-            for (int i = 0; i < window.getCount(); i++) {
+            /*for (int i = 0; i < window.getCount(); i++) {
                 SensorData data = window.getData()[i];
                 addSampleOnPlot(data.x, data.y, data.z);
 
                 // TODO: add sample every 20 ms
+            }*/
+
+            synchronized (sensorDataBuffer) {
+                for (int i = 0; i < window.getCount(); i++)
+                    sensorDataBuffer.add(window.getData()[i]);
             }
+        }
+    }
+
+    private static final int MAX_NUM_SAMPLES_TO_PLOT = 15;
+    private long lastTimeAddSample = System.currentTimeMillis();
+    private class DisplayOneSample implements Runnable {
+        @Override
+        public void run() {
+            long now = System.currentTimeMillis();
+            int numSamplesToAdd = (int)((now - lastTimeAddSample) / 20);
+            if (numSamplesToAdd > MAX_NUM_SAMPLES_TO_PLOT) {
+                lastTimeAddSample = now;
+                numSamplesToAdd = MAX_NUM_SAMPLES_TO_PLOT;
+            }
+            else {
+                lastTimeAddSample += numSamplesToAdd * 20;
+            }
+            synchronized (sensorDataBuffer) {
+                while (numSamplesToAdd > 0) {
+                    if (sensorDataBuffer.size() > 0) {
+                        SensorData data = sensorDataBuffer.get(0);
+                        sensorDataBuffer.remove(0);
+                        addSampleOnPlot(data.x, data.y, data.z);
+                    }
+                    numSamplesToAdd--;
+                }
+            }
+
+            if (!stopFlag)
+                addSampleHandler.postDelayed(new DisplayOneSample(), 20L);
         }
     }
 }
